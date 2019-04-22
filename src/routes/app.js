097198cfd5caba9d38ee631c81dc8b488fa5996c
Fel06 <Fel06@users.noreply.github.com>
2019-04-21 23:03:31 -0500

    terceraa entrega

---
+++ src/routes/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const body = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+
+require('../helpers');
+
+const directoriopublico = path.join(__dirname,'../../public');
+const dirNode_modules = path.join(__dirname , '../node_modules')
+
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+
+app.use(express.static(directoriopublico));
+
+app.use(body.urlencoded({extended:false}));
+
+app.use(require('./index.js'));
+
+mongoose.connect('mongodb://localhost:27017/continua', {useNewUrlParser: true}, (err,resultado) => {
+
+if (err) {
+  return console.error(err);
+}
+
+console.log('Conectado a la base de datos');
+});
+
+app.listen(3000, () => {console.log('Escuchando en el puerto:'+port);
+
+
+  });
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ const hbs = require('hbs');
 const path = require('path');
 const body = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
 
-require('./helpers');
 
-const directoriopublico = path.join(__dirname,'../public');
-const directoriopartials = path.join(__dirname,'../partials');
-const dirNode_modules = path.join(__dirname , '../node_modules')
+require('../helpers');
+
+
+const directoriopartials = path.join(__dirname,'../../partials');
+const dirNode_modules = path.join(__dirname , '../../node_modules')
 
 app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
 app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
@@ -17,7 +17,7 @@ app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
 
 app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
 
-app.use(express.static(directoriopublico));
+
 hbs.registerPartials(directoriopartials);
 app.set('view engine', 'hbs');
 app.use(body.urlencoded({extended:false}));
@@ -76,9 +76,4 @@ app.post('/crear', function(req, res){
                                           descripcion: req.body.desp})
 });
 
-
-
-app.listen(3000, () => {console.log('Escuchando en el puerto:'+port);
-
-
-  });
+module.exports = app
